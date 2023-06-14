b50a6ddaa495ccdd761de3ada974fd0fcb085cdc
kuantele <kuantele@usc.edu>
2023-06-14 12:10:45 -0700

    訂單功能 - 查詢訂單列表

--- src/main/java/com/johnnylee/springbootmall/dao/OrderDao.java
+++ src/main/java/com/johnnylee/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.johnnylee.springbootmall.dao;
 
+import com.johnnylee.springbootmall.dto.OrderQueryParams;
 import com.johnnylee.springbootmall.model.Order;
 import com.johnnylee.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
--- src/main/java/com/johnnylee/springbootmall/service/OrderService.java
+++ src/main/java/com/johnnylee/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.johnnylee.springbootmall.service;
 
 import com.johnnylee.springbootmall.dto.CreateOrderRequest;
+import com.johnnylee.springbootmall.dto.OrderQueryParams;
 import com.johnnylee.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
